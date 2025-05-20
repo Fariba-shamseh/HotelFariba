905f6ca7829197b620dc5b48a7f836637daa90d4
Fariba Shamseh <fariba.shamseh0@gmail.com>
2025-05-20 21:41:04 +0330

    feat: add active state styling to Filter buttons

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
+import { clsx } from "clsx";
 
 const Filter = ({ filterField, options }) => {
   const [searchParams, setSearchParams] = useSearchParams();
+
+  const currentFilter = searchParams.get(filterField) || options.at(0).value; // مقدار فعلی فیلتر
 
   function handleClick(value) {
     searchParams.set(filterField, value);
@@ -35,8 +38,13 @@ const Filter = ({ filterField, options }) => {
       {options.map((option) => (
         <button
           key={option.value}
-          className="bg-gradient-to-r from-[#e74c3c] to-[#c0392b] text-white px-4 py-2 rounded-lg mt-4 hover:from-[#f19a8e] hover:to-[#e57366] transition-all duration-200"
           onClick={() => handleClick(option.value)}
+          className={clsx("px-4 py-2 rounded-lg transition-all duration-200", {
+            "bg-gradient-to-r from-[#e74c3c] to-[#c0392b] text-white":
+              currentFilter === option.value, // حالت فعال: قرمز
+            "border border-gray-300 bg-transparent text-gray-700 hover:bg-gray-100":
+              currentFilter !== option.value, // حالت غیرفعال: بوردری با متن خاکستری
+          })}
         >
           {option.label}
         </button>
